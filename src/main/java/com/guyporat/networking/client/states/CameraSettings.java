cb0ef3aaa8b145dc4f5e78323632f0c440cd3923
Guy Porat <guyporatmail@gmail.com>
2024-02-17 14:03:39 +0200

    Change camera serialization names to match convention

--- src/main/java/com/guyporat/networking/client/states/CameraSettings.java
+++ src/main/java/com/guyporat/networking/client/states/CameraSettings.java
@@ -1,12 +1,18 @@
 package com.guyporat.networking.client.states;
 
+import com.google.gson.annotations.SerializedName;
+
 import java.util.UUID;
 
 public class CameraSettings extends DeviceSettings {
 
+    @SerializedName("camera_name")
     private String name;
 
+    @SerializedName("face_recognition")
     private boolean faceRecognition;
+
+    @SerializedName("target_door_lock")
     private UUID targetDoorLock;
 
     public CameraSettings(String name, boolean faceRecognition, UUID targetDoorLock) {
